52acb8ea1214767abe2f122465c3978f3adef316
Dan Yan <trilodi@gmail.com>
2026-04-26 14:57:55 -0400

    added host permissions

--- chrome-extension/config.js
+++ chrome-extension/config.js
@@ -1,4 +1,6 @@
 // Swap this URL when deploying:
-// Local:  http://localhost:3009
-// Vercel: https://your-project.vercel.app
-const AIN_SERVER_URL = 'http://localhost:3009';
+// Prod
+const AIN_SERVER_URL = 'https://generate-medical-notes-for-jane.vercel.app';
+
+// Dev
+// const AIN_SERVER_URL = 'http://localhost:3009';
